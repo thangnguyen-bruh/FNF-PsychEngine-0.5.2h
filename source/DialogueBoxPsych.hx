62e65fce19c7f092835c3cb9150c496f67121f26
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-11-24 22:32:07 +0200

    Update DialogueBoxPsych.hx

--- source/DialogueBoxPsych.hx
+++ source/DialogueBoxPsych.hx
@@ -93,11 +93,11 @@ class DialogueCharacter extends FlxSprite
 		#if MODS_ALLOWED
 		var path:String = Paths.modFolders(characterPath);
 		if (!FileSystem.exists(path)) {
-			path = Paths.getPreloadPath(characterPath);
+			path = SUtil.getStorageDirectory() + Paths.getPreloadPath(characterPath);
 		}
 
 		if(!FileSystem.exists(path)) {
-			path = Paths.getPreloadPath('images/dialogue/' + DEFAULT_CHARACTER + '.json');
+			path = SUtil.getStorageDirectory() + Paths.getPreloadPath('images/dialogue/' + DEFAULT_CHARACTER + '.json');
 		}
 		rawJson = File.getContent(path);
 
